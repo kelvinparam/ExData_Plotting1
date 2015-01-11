0e3184f387c324ba7b2d2f859dee53211b8c7b3d
Kelvin Param <kelvin.param@gmail.com>
2015-01-12 06:37:54 +1100

    Exploratory Data Analysis - Course Project 1 - more comment lines

--- plot2.R
+++ plot2.R
@@ -2,6 +2,9 @@
 #plot2.R
 #DT3 (data table) contains rows for between 1 and 2 Feb 2007 (inclusive) - see GetnClean.R.
 #GetnClean.R must be run before plot1.R, plot2.R, plot3.R, plot4.R.
+#Please read GetnClean.R before you grade this file. 
+#Converting the Date string and Time string to the appropriate formats takes place in GetnClean.R.
+#Subsetting rows to between 1 and 2 Feb 2007 takes place in GetnClean.R resulting in DT3 which is used here.
 
 png(filename="plot2.png", height=480, width=480, 
  bg="transparent")
